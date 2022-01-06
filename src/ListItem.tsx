9d025c6285db1cc53c8748388e627676d52c5c5c
DashDeipayan <deipayan.dash8@gmail.com>
2022-01-06 20:29:39 +0530

    gradle

--- src/ListItem.tsx
+++ src/ListItem.tsx
@@ -1,29 +1,30 @@
-import React, {FC} from 'react';
-import {Text, View, StyleSheet, Pressable} from 'react-native';
+import React, { FC } from "react";
+import { Text, View, StyleSheet, Pressable } from "react-native";
 
 interface Props {
-  items: number[];
-  itemRemove: Function;
+	items: number[];
+	itemRemove: Function;
 }
 
-const ListItem: FC<Props> = ({items, itemRemove}) => {
-  return (
-    <Pressable>
-    <View style={styles.listitem}>
-      {items.map((item, index) => (
-        <Text key={index}>{item}</Text>
-      ))}
-    </View>
-  );
+const ListItem: FC<Props> = ({ items, itemRemove }) => {
+	return (
+		<Pressable>
+			<View style={styles.listitem}>
+				{items.map((item, index) => (
+					<Text key={index}>{item}</Text>
+				))}
+			</View>
+		</Pressable>
+	);
 };
 
 const styles = StyleSheet.create({
-  listitem: {
-    backgroundColor: '#cecece',
-    alignItems: 'center',
-    margin: 10,
-    width: '100%',
-  },
+	listitem: {
+		backgroundColor: "#cecece",
+		alignItems: "center",
+		margin: 10,
+		width: "100%",
+	},
 });
 
 export default ListItem;
